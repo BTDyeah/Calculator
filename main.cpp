c9c453e1a03d2ae99526c27ca488cffa5243e79b
BornToDevelop <36602321+BornToDevelop@users.noreply.github.com>
2019-01-19 19:40:34 +0100

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "Calculator.cpp"
+
+int main() 
+{
+	char sign;
+	Calculator obj;
+	std::cout << "Please enter one of the following signs for following operations\n" 
+	<< "+ --> addition\n" << "- --> subtraction\n" << "* --> multiplication\n" 
+	<< "/ --> divison\n" << "p --> power\n" << "s -- square root\n";
+	std::cin >> sign;
+		
+		switch(sign)
+	{
+		case '+':
+			obj.setAdd(); break;
+		case '-':
+			obj.setSub(); break;
+		case '*':
+			obj.setMult(); break;
+		case '/':
+			obj.setDiv(); break;
+		case 'p':
+			obj.setPow(); break;
+		case 's':
+			obj.setSqrt(); break;
+		default: 
+			std::cout << "Please try again entering one of valid characters--> +, -, *, /, p, s\n";			
+						
+	}
+	return 0;
+}
