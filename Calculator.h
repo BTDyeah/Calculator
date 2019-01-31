b1799bdef4ca65b4c5518f5edd8e27b401176005
BornToDevelop <36602321+BornToDevelop@users.noreply.github.com>
2019-01-31 19:55:55 +0100

    default arguments

--- Calculator.h
+++ Calculator.h
@@ -4,12 +4,12 @@
 class Calculator
 {
 	public:
-		void setAdd(double counter);
-		void setSub(double counter);
-		void setMult(double counter);
+		void setAdd(double counter = 0);
+		void setSub(double counter = 0);
+		void setMult(double counter = 1);
 		void setDiv();
-	    void setPow();
-	    void setSqrt();	
+	        void setPow();
+	    	void setSqrt();	
 		
 	
 			
